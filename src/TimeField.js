114b8c2b215db235306ea7551910f4ebe9894ba2
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-02-05 15:05:53 +0900

    시간, 분 input 추가

--- src/TimeField.js
+++ src/TimeField.js
@@ -1,5 +1,65 @@
+import React, { useState, useRef } from 'react';
+
 const TimeField = (props) => {
-	return <h3>테스트</h3>;
+	const [state, setState] = useState({
+		hour: '',
+		minutes: '',
+	});
+	const refs = useRef(new Map());
+
+	const getValid = () => {
+		
+	}
+
+	const handleChange = (target) => {
+		const { id, value } = target;
+		setState((prevState) => ({
+			...prevState,
+			[id]: value,
+		}));
+	};
+
+	const handleKyeDown = (e) => {
+		if (e.key !== 'ArrowRight' && e.key !== 'ArrowLeft') return;
+		const {
+			target: { id },
+		} = e;
+		refs.current.get(id).focus();
+	};
+
+	return (
+		<>
+			<div>
+				<label htmlFor="name">tabIndex test 용 input:</label>
+				<input type="text" id="name" name="name" size="10" />
+			</div>
+			<div>
+				<label htmlFor="time">시간 : </label>
+				<div id="time" tabIndex="0">
+					<input
+						id="hour"
+						type="text"
+						value={state.hour}
+						onChange={({ target }) => handleChange(target)}
+						ref={(e) => refs.current.set('minutes', e)}
+						maxLength={2}
+						onKeyDown={handleKyeDown}
+						placeholder="_ _"
+					/>
+					<input
+						id="minutes"
+						type="text"
+						value={state.minutes}
+						onChange={({ target }) => handleChange(target)}
+						ref={(e) => refs.current.set('hour', e)}
+						maxLength={2}
+						onKeyDown={handleKyeDown}
+						placeholder="_ _"
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default TimeField;
