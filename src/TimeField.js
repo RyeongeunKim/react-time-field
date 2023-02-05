3551e6c203634dc92e96e1b5ef4c0d66421dd7a2
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-02-05 15:59:11 +0900

    시간 유효성 검사 및 change 이벤트 로직 추가

--- src/TimeField.js
+++ src/TimeField.js
@@ -7,16 +7,52 @@ const TimeField = (props) => {
 	});
 	const refs = useRef(new Map());
 
-	const getValid = () => {
-		
-	}
+	// 시간 유효성 검사
+	const getValid = (id, value, isHour) => {
+		if (isNaN(value)) return false;
+
+		const numValue = Number(value);
+		const valueLength = value.length;
+		let valid1;
+		let valid2;
+
+		if (isHour) {
+			valid1 = 2;
+			valid2 = 24;
+		} else {
+			if (refs.current.get(id).value === '24' && numValue > 0) {
+				return false;
+			}
+			valid1 = 5;
+			valid2 = 59;
+		}
+
+		if (
+			(valueLength === 1 && numValue > valid1) ||
+			(valueLength === 2 && numValue > valid2)
+		) {
+			return false;
+		} else {
+			return true;
+		}
+	};
 
 	const handleChange = (target) => {
 		const { id, value } = target;
-		setState((prevState) => ({
-			...prevState,
-			[id]: value,
-		}));
+		const inputValue = value.trim();
+		const isHour = id.toLowerCase().includes('hour');
+		const isValid = getValid(id, inputValue, isHour);
+
+		if (isValid) {
+			// 분 포커스
+			if (isHour && inputValue.length === 2) {
+				refs.current.get(id).focus();
+			}
+			setState((prevState) => ({
+				...prevState,
+				[id]: inputValue,
+			}));
+		}
 	};
 
 	const handleKyeDown = (e) => {
@@ -46,6 +82,7 @@ const TimeField = (props) => {
 						onKeyDown={handleKyeDown}
 						placeholder="_ _"
 					/>
+					:
 					<input
 						id="minutes"
 						type="text"
